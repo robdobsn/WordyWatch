838040612dc66c0cc0145490f2ec468ff674579d
Rob Dobson <rob@dobson.com>
2026-04-09 16:45:50 -0400

    Removed support for older watch hw

--- WordyFW/systypes/WordyWatchV10/features.cmake
+++
@@ -1,9 +0,0 @@
-# Include common features
-include("${BUILD_CONFIG_DIR}/../Common/features.cmake")
-
-# add_compile_definitions(FEATURE_GAMES)
-# add_compile_definitions(FEATURE_WRIST_TILT)
-# add_compile_definitions(FEATURE_BATTERY_GAUGE)
-
-set(DEV_TYPE_JSON_FILES "${BUILD_CONFIG_DIR}/DevTypes.json")
-
